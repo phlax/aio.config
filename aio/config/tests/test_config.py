e8f248708902ecf1bf833e23d0f79ee2b968d83e
Ryan Northey <ryan@space23.net>
2015-05-11 09:13:01 +0000

    adding test for string config

--- aio/config/tests/test_config.py
+++ aio/config/tests/test_config.py
@@ -6,11 +6,24 @@ from aio.config import parse_config, find_config
 
 TEST_DIR = os.path.dirname(__file__)
 
+CONFIG_STRING = """
+[section1]
+bar: 1
+foo: 2
 
-class AioConfigTestCase(unittest.TestCase):
+[section2]
+bar: 3
+foo: 4
+"""
+
+
+class AioConfigParseTestCase(unittest.TestCase):
 
     @aiotest
     def test_parse_config(self):
+        """
+        this test should find and parse "aio.conf" in the app_dir
+        """
         app_dir = os.path.join(
             TEST_DIR, "resources")
         config = yield from parse_config(app_dir=app_dir)
@@ -19,6 +32,9 @@ class AioConfigTestCase(unittest.TestCase):
 
     @aiotest
     def test_parse_custom_config(self):
+        """
+        this test should parse a custom config file
+        """
         configfile = os.path.join(
             TEST_DIR, "resources", "test-1.conf")
         config = yield from parse_config(configfile)
@@ -26,7 +42,20 @@ class AioConfigTestCase(unittest.TestCase):
         self.assertEquals(config["foo"]["bar"], "1")
         self.assertEquals(config["bar"]["foo"], "baz")
 
+    @aiotest
+    def test_config_string(self):
+        config = yield from parse_config(config_string=CONFIG_STRING)
+        self.assertEquals(config.sections(), ["section1", "section2"])
+        self.assertEquals(config["section1"]["foo"], "2")
+        self.assertEquals(config["section2"]["bar"], "3")
+
+
+class AioConfigFinderTestCase(unittest.TestCase):
+
     def test_find_config(self):
+        """
+        this test should find "aio.conf" in the app_dir
+        """
         app_dir = os.path.join(
             TEST_DIR, "resources")
         self.assertEquals(
@@ -35,6 +64,9 @@ class AioConfigTestCase(unittest.TestCase):
                 app_dir, 'aio.conf'))
 
     def test_find_config_etc(self):
+        """
+        this test should find "aio.conf" in the etc
+        """
         app_dir = os.path.join(
             TEST_DIR, "resources", "sub")
         self.assertEquals(
