e98c0b922456b1a3b234e9614584ff464d655a5e
Ryan Northey <ryan@space23.net>
2015-05-19 08:10:27 +0000

    package_data

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     packages=find_packages(),
     namespace_packages=['aio'],
     include_package_data=True,
-    package_data={'': ['aio/config/tests/resources/*.conf', 'tests/resources/*.conf', '*.rst']},    
+    package_data={'': ['aio/config/tests/resources/*.conf', '*.rst']},    
     zip_safe=False,
     tests_require=tests_require,
     test_suite="aio.config.tests",
